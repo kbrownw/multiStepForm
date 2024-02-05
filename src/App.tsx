17ebbe492cc49b7713afb97d765632da0c3a9462
Keith Brown <kbrownw@gmail.com>
2024-02-05 14:46:45 -0500

    Add box shadows to section and header/form area

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,14 @@ function App() {
       {/* MAIN CONTAINER */}
       <section
         className="flex flex-col h-full bg-mobile-sidebar bg-top bg-no-repeat bg-contain
-      md:grid md:grid-cols-[1fr_3fr] md:bg-white md:bg-none md:w-[1200px] md:h-[750px] md:p-5 md:rounded-2xl"
+      md:grid md:grid-cols-[1fr_3fr] md:bg-white md:bg-none md:w-[1200px] md:h-[750px] md:p-5 md:rounded-2xl
+      md:shadow-lg"
       >
         {/* SIDEBAR */}
         <Sidebar isLargeScreen={isLargeScreen} />
         {/* TEXT AND FORM AREA */}
         <div className="grid h-full md:px-28">
-          <div className="w-[90%] mx-auto p-5 rounded-lg bg-white md:w-full">
+          <div className="w-[90%] mx-auto p-5 rounded-lg bg-white shadow-lg md:w-full md:shadow-none">
             {/* TITLE AND SUBTITLE HERE */}
             <Heading />
             {/* INPUT FIELDS */}
